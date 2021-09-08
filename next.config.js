cf04eedcdef0cf02b2cf7fd6698d7a61dd1d06e0
Jehovah Yii Zui Hon (Hovah) <31881261+hovahyii@users.noreply.github.com>
2021-09-08 11:06:33 +0800

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ module.exports = {
 		path: "https://a.staticaa.com/images",
 	},
 	productionBrowserSourceMaps: true,
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 }
